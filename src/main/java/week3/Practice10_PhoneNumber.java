b9280db967b427aabbd905f3d8b3454ce91e7584
Premkumar Divakaran <premkumar.divakaran@oracle.com>
2022-01-07 22:10:08 +0530

    week4

--- src/main/java/week3/Practice10_PhoneNumber.java
+++ src/main/java/week3/Practice10_PhoneNumber.java
@@ -7,7 +7,12 @@ import java.util.*;
 public class Practice10_PhoneNumber {
     /*
     Pseudocode -
-
+    1. Store numbers & character in Map
+    2. Create function to find combination for two list
+    3. Start with first 2 digits, find possible combinations and store it in list
+    4. Iterate with remaining digits, pass list from step 3 and next digit
+    5. Store result in same list
+    6. Return list
      */
 
     //Input
@@ -25,16 +30,16 @@ public class Practice10_PhoneNumber {
 
     @Test  //Positive
     public void example1(){
+        System.out.println(letterCombinations("7296"));
     }
 
     @Test //Negative
     public void example2(){
-
+        System.out.println(letterCombinations(""));
     }
 
     @Test //Edge
     public void example3(){
-        System.out.println(letterCombinations(""));
         System.out.println(letterCombinations("5"));
     }
 
@@ -42,12 +47,23 @@ public class Practice10_PhoneNumber {
         List<String> out = new ArrayList<>();
 
         if(digits.length()==0) return out;
-        else if(digits.length()==1) out = input.get(digits);
-        else{
+        else if(digits.length()==1) return input.get(digits);
 
-        }
+        out = findCombinations(input.get(String.valueOf(digits.charAt(0))),
+                input.get(String.valueOf(digits.charAt(1))));
+
+        for(int i=2;i<digits.length();i++)
+            out = findCombinations(out,input.get(String.valueOf(digits.charAt(i))));
 
         return out;
+    }
+
+    private static List<String> findCombinations(List<String> ls1, List<String> ls2){
+        List<String> combinations = new ArrayList<>();
+        for(int i=0;i<ls1.size();i++)
+            for(int j=0;j<ls2.size();j++) combinations.add(ls1.get(i)+ls2.get(j));
+
+        return combinations;
     }
 
 }
--- src/main/java/week4/Practice3_findDnaSequence.java
+++ src/main/java/week4/Practice3_findDnaSequence.java
@@ -50,6 +50,7 @@ public class Practice3_findDnaSequence {
 
             left++;
             right++;
+
         }
 
         return out;
